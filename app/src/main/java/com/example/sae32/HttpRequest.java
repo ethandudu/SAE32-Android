9f0b1675d00c5610bc48694aa870b9192b5fa036
Ethan <41381482+ethandudu@users.noreply.github.com>
2023-12-09 18:14:45 +0100

    Fixed intent call when selecting a test + added javadoc + add catchup in case of error

--- app/src/main/java/com/example/sae32/HttpRequest.java
+++ app/src/main/java/com/example/sae32/HttpRequest.java
@@ -9,31 +9,44 @@ import java.net.HttpURLConnection;
 import java.net.URL;
 
 
+/**
+ * Do a HTTP request to the server, and return the response.
+ * Takes as parameter the url and the method
+ */
 public class HttpRequest extends AsyncTask<String, Void, String> {
     @Override
-    protected String doInBackground(String... params) {
+    protected String doInBackground(String ... params){
         String url = params[0];
+        String method = params[1];
         String response = "";
         try {
             URL obj = new URL(url);
             HttpURLConnection con = (HttpURLConnection) obj.openConnection();
-            con.setRequestMethod("GET");
+            con.setRequestMethod(method);
             con.setRequestProperty("Authorization", "wJb~9E@ttr#{%?K7");
             con.setRequestProperty("User-Agent", "Android/SAE32-App");
             int responseCode = con.getResponseCode();
 
-            //read the response
-            BufferedReader in = new BufferedReader(new InputStreamReader(con.getInputStream()));
-            String inputLine;
-            StringBuilder responseBuffer = new StringBuilder();
-            while ((inputLine = in.readLine()) != null) {
-                responseBuffer.append(inputLine);
+            if (responseCode == HttpURLConnection.HTTP_OK) {
+                //read the response
+                BufferedReader in = new BufferedReader(new InputStreamReader(con.getInputStream()));
+                String inputLine;
+                StringBuilder responseBuffer = new StringBuilder();
+                while ((inputLine = in.readLine()) != null) {
+                    responseBuffer.append(inputLine);
+                }
+                in.close();
+                response = responseBuffer.toString();
+            } else {
+                response = "Error: " + responseCode;
             }
-            in.close();
-            response = responseBuffer.toString();
 
 
-        } catch (IOException e) { e.printStackTrace(); }
+
+        } catch (IOException e) {
+            e.printStackTrace();
+            response = "Error: " + e.getMessage();
+        }
         return response;
     }
 }
--- app/src/main/java/com/example/sae32/MainActivity.java
+++ app/src/main/java/com/example/sae32/MainActivity.java
@@ -20,6 +20,8 @@ import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
 
+    private String[] tests;
+
     @SuppressLint("MissingInflatedId")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -32,41 +34,59 @@ public class MainActivity extends AppCompatActivity {
     }
 
     public void onItemClick (AdapterView<?> p, View v, int pos, long id){
-        Intent intent= new Intent(this, ProtocolInfo.class);
-        intent.putExtra("id", pos);
+        //parsing the string to get the id of the test
+        String[] parts = tests[pos].split(" - ");
+        String idTest = parts[0];
+        Intent intent= new Intent(this, ListePaquets.class);
+        intent.putExtra("id", idTest);
         startActivity(intent);
     }
 
 
+    /**
+     * @param view the view that called the method
+     */
     public void refreshData(View view) {
         Toast.makeText(this, "Actualisation ...", Toast.LENGTH_SHORT).show();
 
-        String url = "https://api.sae32.ethanduault.fr/tests.php";
+        //prepare the request
+        String url = getString(R.string.testUrl);
         HttpRequest request = new HttpRequest();
         String response = "";
 
         try {
-            response = request.execute(url).get();
-        } catch (Exception e) { e.printStackTrace(); }
+            response = request.execute(url, "GET").get();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+        if (response.startsWith("Error")) {
+            Toast.makeText(this, response, Toast.LENGTH_SHORT).show();
+            return;
+        }
 
         ArrayList<String> items = new ArrayList<>();
         ArrayAdapter<String> adapter;
 
         try {
+            //read the json
             JSONObject json = new JSONObject(response);
             JSONArray array = json.getJSONArray("data");
-            String[] tests = new String[array.length()];
+            tests = new String[array.length()];
             for (int i = 0; i < array.length(); i++) {
                 JSONObject item = array.getJSONObject(i);
                 String id = item.getString("ID");
                 String name = item.getString("name");
-                //add to array
                 tests[i] = id + " - " + name;
             }
 
+            //add the data to the list and display it
             adapter = new ArrayAdapter<>(this, android.R.layout.simple_list_item_1, tests);
             ListView laListe = findViewById(R.id.idListeView);
             laListe.setAdapter(adapter);
-        } catch (JSONException e) { e.printStackTrace(); }
+
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
     }
 }
