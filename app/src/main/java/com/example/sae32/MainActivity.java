a40861e95f58d49a27bebde1c8586ee671011b64
Ethan <41381482+ethandudu@users.noreply.github.com>
2023-12-09 13:45:10 +0100

    Removed unsued imports, added function to load data at startup and refresh when click on the refresh button, added org.json imports

--- app/src/main/java/com/example/sae32/MainActivity.java
+++ app/src/main/java/com/example/sae32/MainActivity.java
@@ -8,16 +8,17 @@ import android.os.Bundle;
 import android.content.Intent;
 import android.view.View;
 import android.widget.AdapterView;
-import android.widget.AdapterView.OnItemClickListener;
 import android.widget.ArrayAdapter;
-import android.widget.EditText;
 import android.widget.ListView;
+import android.widget.Toast;
 
-import com.example.sae32.HttpRequest;
+import org.json.JSONArray;
+import org.json.JSONObject;
+import org.json.JSONException;
+
+import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
-
-    private ListView laListe;
 
     @SuppressLint("MissingInflatedId")
     @Override
@@ -25,20 +26,47 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        laListe= findViewById(R.id.idListeView);
-        laListe.setOnItemClickListener(this::onItemClick);//onItemClick);
-
-        String url = "https://api.sae32.ethanduault.fr/tests.php";
-        HttpRequest request = new HttpRequest();
-        String response = "";
-        try {
-            response = request.execute(url).get();
-        } catch (Exception e) { e.printStackTrace(); }
-        System.out.println("Response get " + response);
+        ListView laListe = findViewById(R.id.idListeView);
+        laListe.setOnItemClickListener(this::onItemClick);
+        refreshData(findViewById(R.id.refresh));
     }
 
     public void onItemClick (AdapterView<?> p, View v, int pos, long id){
         Intent intent= new Intent(this, ProtocolInfo.class);
+        intent.putExtra("id", pos);
         startActivity(intent);
+    }
+
+
+    public void refreshData(View view) {
+        Toast.makeText(this, "Actualisation ...", Toast.LENGTH_SHORT).show();
+
+        String url = "https://api.sae32.ethanduault.fr/tests.php";
+        HttpRequest request = new HttpRequest();
+        String response = "";
+
+        try {
+            response = request.execute(url).get();
+        } catch (Exception e) { e.printStackTrace(); }
+
+        ArrayList<String> items = new ArrayList<>();
+        ArrayAdapter<String> adapter;
+
+        try {
+            JSONObject json = new JSONObject(response);
+            JSONArray array = json.getJSONArray("data");
+            String[] tests = new String[array.length()];
+            for (int i = 0; i < array.length(); i++) {
+                JSONObject item = array.getJSONObject(i);
+                String id = item.getString("ID");
+                String name = item.getString("name");
+                //add to array
+                tests[i] = id + " - " + name;
+            }
+
+            adapter = new ArrayAdapter<>(this, android.R.layout.simple_list_item_1, tests);
+            ListView laListe = findViewById(R.id.idListeView);
+            laListe.setAdapter(adapter);
+        } catch (JSONException e) { e.printStackTrace(); }
     }
 }
