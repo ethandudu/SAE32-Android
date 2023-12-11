302f4b8c945761ff639c58716e144f1bc32fdbbc
Ethan <41381482+ethandudu@users.noreply.github.com>
2023-12-11 11:21:06 +0100

    Added Return function

--- app/src/main/java/rt/sae32/android/ProtocolInfo.java
+++ app/src/main/java/rt/sae32/android/ProtocolInfo.java
@@ -3,6 +3,7 @@ package rt.sae32.android;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
 
 public class ProtocolInfo extends AppCompatActivity {
 
@@ -10,5 +11,9 @@ public class ProtocolInfo extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_protocol_info);
+    }
+
+    public void returnToPreviousActivity(View view){
+        super.finish();
     }
 }
