01c9c7e77adb703714d49fe4124803e8539bfa0e
Ethan <41381482+ethandudu@users.noreply.github.com>
2023-12-18 22:02:25 +0100

    Removed downloadOuiFile function => now in the API

--- app/src/main/java/rt/sae32/android/Settings.java
+++ app/src/main/java/rt/sae32/android/Settings.java
@@ -24,7 +24,6 @@ public class Settings extends AppCompatActivity {
         }
         EditText serverUrl = findViewById(R.id.serverUrl);
         serverUrl.setText(sharedPreferences.getString("serverUrl", ""));
-        System.out.println(sharedPreferences.getBoolean("ouiDownloaded", false));
 
     }
     public void returnToMainActivity(View view){
@@ -42,32 +41,5 @@ public class Settings extends AppCompatActivity {
         super.finish();
     }
 
-    public void macSwitchChange(View v){
-        SwitchMaterial macResolution = findViewById(R.id.macresolution);
-        if (macResolution.isChecked()){
-            SharedPreferences sharedPreferences = getSharedPreferences("settings", MODE_PRIVATE);
-            if (!sharedPreferences.getBoolean("ouiDownloaded", false)) {
-                System.out.println("oui");
-                //show a dialog to ask if the user wants to download the oui file
-                AlertDialog.Builder builder = new AlertDialog.Builder(this);
-                builder.setMessage("Pour utiliser la résolution du constructeur de l'adresse MAC, le téléchargement de la base de données est requis. Voulez-vous le faire maintenant ?")
-                        .setPositiveButton("Oui", (dialog, id) -> {
-                            SharedPreferences.Editor editor = sharedPreferences.edit();
-                            editor.putBoolean("ouiDownloaded", true);
-                            editor.apply();
-                            DownloadOuiFile();
-                        })
-                        .setNegativeButton("Non", (dialog, id) -> {
-                            macResolution.setChecked(false);
-                        });
-                AlertDialog dialog = builder.create();
-                dialog.show();
-            }
-        }
-    }
-    public void DownloadOuiFile(){
-        DownloadFile downloadFile = new DownloadFile();
-        downloadFile.execute("https://api.sae32.ethanduault.fr/oui.json", getFilesDir().getAbsolutePath() + "/oui.json");
 
-    }
 }
